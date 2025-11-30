b7764c86c35443b9e327f4d66751bde5f7683df0
sungjuGit <sungtaek.ju@outlook.com>
2025-11-30 11:31:10 -0800

    modified setup.py for v0.2

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="svgp_kan",
-    version="0.1.0",
-    description="Scalable Probabilistic Kolmogorov-Arnold Networks using Sparse Variational GPs",
+    version="0.2.0",  # Updated for Vision Support
+    description="Scalable Probabilistic KANs & U-Nets (Vision/Tabular) using Sparse Variational GPs",
     author="Open Source Contributor",
+    url="https://github.com/sungjuGit/svgp-kan",  # Added your repo link
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0",
@@ -12,12 +13,20 @@ setup(
         "matplotlib",
         "scikit-learn"
     ],
-
     python_requires='>=3.8',
-    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",      # New
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",  # New (U-Net context)
+    ],
+    keywords=[
+        "KAN", 
+        "Kolmogorov-Arnold Networks", 
+        "Gaussian Processes", 
+        "U-Net", 
+        "Computer Vision", 
+        "Uncertainty Quantification"
     ],
 )
